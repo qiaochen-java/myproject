b2b9887774dcd0dd8b1840836337803ce7ad97d5
qiaochen <854159292@qq.com>
2016-07-29 00:50:22 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,13 +1,6 @@
 public class Hello {
-	public static final String COMPANY_NAME="Yootk";
 	public static void main(String args[]){	
-		System.out.println("Happy New Year");
-		System.out.println("Happy MySelf");
-		print();
+		System.out.println("************");
+		System.out.println("#############");
 	} 
-	public static void print(){
-		System.out.println("****************");
-		System.out.println("* Hello Word ! *");
-		System.out.println("****************");
-	}
 }
